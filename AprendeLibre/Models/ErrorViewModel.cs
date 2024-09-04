2f0cf38577a224ba4b2329bae7621c6cbd2cd993
SantiiCol <santiiagoverde10@gmail.com>
2024-09-03 20:56:00 -0500

    Arquitectura del software

---
+++ AprendeLibre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AprendeLibre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
